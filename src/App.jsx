cca68f99e0f1c524b41e6bf8e94dd35f34e7c02a
Leilanigreer <leilani@lpcgolf.com>
2024-09-17 18:31:28 -0700

    created an array with hashs for the data and then made it dynamic on the front end

--- src/App.jsx
+++ src/App.jsx
@@ -37,21 +37,48 @@ function PostsNew() {
 }
 
 // I need to add more to this:
-function PostsIndex() {
+function PostsIndex(props) {
+console.log(props);  
   return (
     <div id="posts-index">
     <h1>All posts</h1>
-    <h2>Much to do about nothing</h2>
-    <body> Do you know what this is about?</body>
-  </div>
+    {props.posts.map(post => (
+      <div key={post.id} className="posts">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <img src={post.image} alt="" />
+      </div>
+    ))}
+    </div>
   )
 }
 
 function PostsPage() {
+  // var name = "Leilani";
+  let posts = [
+    {
+      id: 1, 
+      title: "Hello Dolly", 
+      body: "First Musical I ever saw", 
+      image: "https://i.ytimg.com/vi/LnXf6tmczzo/maxresdefault.jpg"
+    }, 
+    {
+      id: 2, 
+      title: "RENT", 
+      body: "Most awkward musical seen with my mother",
+      image: "https://i.ebayimg.com/images/g/~FEAAOSw9FhlMbeP/s-l400.jpg"
+    }, 
+    {
+      id: 3, 
+      title: "Legally Blonde the musical",
+      body: "Most recent musical", 
+      image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F820222679%2F1566026794663%2F1%2Foriginal.20240804-164615?w=1000&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=b07c8ad645eb207ca039ef83c7c36a31"
+    }
+  ];
   return (
     <main>
       <PostsNew />
-      <PostsIndex />
+      <PostsIndex posts={posts} />
     </main>
   )
 }
